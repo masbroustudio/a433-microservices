13074142ed05ab81eeda032d5458e2639814cb4e
Yudha E <masbroumail@gmail.com>
2024-02-06 01:23:02 +0700

    .

--- build_push_image.sh
+++ build_push_image.sh
@@ -1,10 +1,28 @@
 #!/bin/bash
 
-# Membangun image Docker
-docker build -t ghcr.io/masbroustudio/shipping-service:latest .
+# Memeriksa apakah Docker sudah terinstal pada sistem
+if command -v docker &> /dev/null; then
+    
+    # Membangun image Docker dengan tag masbroustudio/shipping-service:latest
+    docker build -t masbroustudio/shipping-service:latest .
 
-# Masuk ke GitHub Container Registry
-echo $GH_PACKAGES_TOKEN | docker login ghcr.io -u masbroustudio --password-stdin
+    # Menampilkan daftar image Docker yang tersedia di lokal
+    docker images
 
-# Mengunggah image Docker ke GitHub Container Registry
-docker push ghcr.io/masbroustudio/shipping-service:latest
+    # Menentukan nama paket Docker untuk GitHub Packages
+    DOCKER_PACKAGE="ghcr.io/masbroustudio/shipping-service:latest"
+
+    # Mengubah tag image Docker agar sesuai dengan format GitHub Packages
+    docker tag masbroustudio/shipping-service:latest $DOCKER_PACKAGE
+
+    # Melakukan login ke GitHub Packages menggunakan token akses pribadi (PAT)
+    echo $GHCR_PAT | docker login ghcr.io -u masbroustudio --password-stdin
+
+    # Mengunggah image Docker ke GitHub Packages
+    docker image push $DOCKER_PACKAGE
+
+else
+    # Menampilkan pesan kesalahan jika Docker belum terinstal dan menghentikan skrip
+    echo "Docker belum terinstal. Silakan instal Docker sebelum menjalankan skrip ini."
+    exit 1
+fi
